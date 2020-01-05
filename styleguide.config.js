f0b22a2f496ac19146dd511202566057a39e5201
henriquesml <henrique_schmeller@hotmail.com>
2020-01-05 15:13:18 -0300

    melhorando layout

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,13 +1,35 @@
 module.exports = {
-    webpackConfig: {
-      module: {
-        rules: [
-          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-          }
-        ]
-      }
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        }
+      ]
+    }
+  },
+  template: {
+    favicon: 'https://api.adorable.io/avatars/285/abott@adorable.pngds',
+    head: {
+      links: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css?family=Roboto'
+        }
+      ]
+    }
+  },
+  theme: {
+    color: {
+      link: '#333',
+      linkHover: '#29bff5 '
     },
-  };
+    fontFamily: {
+      base: '"Roboto", sans-serif',
+    }
+  },
+  title: "Artia UI",
+  version: "0.0.1",
+};
